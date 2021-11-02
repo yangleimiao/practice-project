f07edc3b2921178e19a24a3aec3cce6d097102bd
杨磊 <yanglei1@ifeng.com>
2021-11-02 11:24:41 +0800

    prac-mybatis

---
+++ prac-mybatis/src/main/java/com/example/mybatis/config/HttpConfig.java
@@ -0,0 +1,12 @@
+package com.example.mybatis.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author yanglei
+ * @date 2021/10/11
+ */
+@Configuration
+public class HttpConfig {
+
+}
